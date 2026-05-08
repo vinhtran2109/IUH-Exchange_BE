eb277cdc57291fe69bfadf55cb827b54667f2c3b
IUH Exchange Migration <iuh-exchange@nodejs.migration>
2026-05-08 18:52:12 +0800

    refactor: remove Spring Boot code, move Node.js to root
    - Deleted all Java/Spring Boot services (92 .java files)
    - Deleted Maven wrapper, pom.xml, config-server, service-registry
    - Moved node-services/* to project root
    - Updated .gitignore for Node.js

--- product-service/src/main/java/edu/iuh/exchange/productservice/api/dto/UploadUrlRequest.java
+++
@@ -1,6 +0,0 @@
-package edu.iuh.exchange.productservice.api.dto;
-
-public record UploadUrlRequest(
-        String filename,
-        String contentType
-) {}
--- product-service/src/main/java/edu/iuh/exchange/productservice/domain/model/ProductCondition.java
+++
@@ -1,9 +0,0 @@
-package edu.iuh.exchange.productservice.domain.model;
-
-public enum ProductCondition {
-    NEW,
-    LIKE_NEW,
-    GOOD,
-    FAIR,
-    POOR
-}
